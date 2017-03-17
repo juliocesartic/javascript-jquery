47a83d68c8ebe51a9fb6addda2b090587fb1c1d1
Julio Jaramillo <juliocesar0293@gmail.com>
2017-03-16 18:38:40 -0600

    Primer avance: Instalacion Jquery y creacion de html

---
+++ tvify/index.js
@@ -0,0 +1,28 @@
+/*window.onload = function(){
+  alert('loaded');
+}*/
+
+ /*$(document).ready(function(){
+   alert('Esta lista la ejecucion')
+ })*/
+
+/*
+--Cuando tienes mas de un js usando $
+$.noConflict();
+jQuery(document).ready(function ($){})
+*/
+$(function(){
+  //var header = document.getElementById('app-header');
+  //console.log(header);
+
+  //var header = $('header');
+  //var title = $('h1', header[0]);
+  //console.log(title);
+
+  //var header = document.getElementById('app-header');
+  // var $header = $(header);
+  //var $header = $(header);
+  //console.log($header);
+
+  var headings = $('h1, h2');
+});
